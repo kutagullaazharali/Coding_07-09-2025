4d1de9365454ee3d8f40fcc65de0b9dc8e55415f
Azhar Ali <azharali87901@gmail.com>
2026-02-05 23:57:46 +0530

    New Program

--- MailValidation.java
+++ MailValidation.java
@@ -1,30 +1,30 @@
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MailValidation {
-   static public int numUniqueEmails(String[] emails) {
+    static public int numUniqueEmails(String[] emails) {
        int count = 0;
-       ArrayList<String> arr = new ArrayList<>();
-       for (String s : emails) {
-
-           s = s.replace(".", "");
-           if (s.contains("+")) {
-
-               int indexplus = s.indexOf("+");
-               int indexat = s.indexOf("@");
-               
-           }
-           
-           //System.out.println(Arrays.toString(str));
-           StringBuilder sb = new StringBuilder();
-           for (int i = 0; i < str.length; i++) {
-
-           }
-           arr.add(sb.toString());
-        }
-        return count;
+        Set<String> set = new HashSet<>();
+        for (String s : emails) {
+            String str[] = s.split("@");
+            str[0] = str[0].replace(".", "");
+            str[0] += "@";
+            s = String.join("", str);
+            if (s.contains("+")) {
+               int plus = s.indexOf("+");
+                int at = s.indexOf('@');
+                if(plus < at) {
+                    String temp = s.substring(plus, at);
+                    s = s.replace(temp, "");
+                }              
+            }
+            set.add(s);
+       }
+    return set.size();
     }
+
     public static void main(String[] args) {
         String s[] = { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com",
                 "testemail+david@lee.tcode.com" };
